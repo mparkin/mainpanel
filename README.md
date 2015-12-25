bbf3ce7e71487b0ba83ca3fe81ac6f5f5ac75728
Mike Parkin <mparkin@gmail.com>
2015-12-25 09:19:57 -0800

    Updated README and Serial message

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-mainpanel.py
+# mainpanel.py
+
+##Description
+
+This is the operator interface to the pump control system. 
+It talks to the pump(s) through a network connetion and to two sensors;
+Pressure sensor and a mass scale. These are connected to the operator
+interface system via an arduino and a serial(USB) connection.
+
+The message from the arduino is in this format <"last mass measurement","AVG mass", "Pressure">
+
+## TO DO
+
+1. Check for connection to pump. If not there send message and die
+ 
+
+
--- senscollect.py
+++ senscollect.py
@@ -2,13 +2,24 @@
 
 import datetime
 import serial
+import telnetlib as Tnet
+
+Hostaddress = "192.168.0.116"
 
 ser = serial.Serial('/dev/ttyACM0',38400)
 filename = "data_{0}".format(datetime.datetime.now().strftime('%d%m%y_%H%M%S'))
 f = open(filename, "w");
+tn = Tnet.Telnet(Hostaddress)
+tn.write("I\n")
+print tn.read_lazy()
+tn.write("B\n")
+print tn.read_lazy()
+
 while 1:
 	data = ser.readline()
         print data
 	print datetime.datetime.now().strftime('%d/%m/%Y %H:%M:%S')
 	f.write("{0},{1}\r\n".format(datetime.datetime.now().strftime('%d/%m/%Y %H:%M:%S'),data))
 f.close()
+tn.write("Z\n")
+tn.close()
