3e7c7eaa6ddcc1e36c82bf89f7668ba345f1ba06
Mike Parkin <mparkin@gmail.com>
2015-12-26 14:08:47 -0800

    beginning of GUI

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.settings import SettingsWithSidebar
+from kivy.uix.label import Label
 
 from settingsjson import settings_json
 
@@ -15,31 +16,61 @@ import telnetlib as Tnet
 
 Builder.load_string('''
 <Interface>:
-    orientation: 'vertical'
-    Button:
-        test: 'open the settings!'
-        font_size: 50
-        on_release: app.open_settings()
+    id: MainWindow
+    orientation: 'horizontal'
+    size: root.size
+    BoxLayout: 
+        orientation: 'vertical'
+        Label: 
+            text: 'Pump Controls'
+        Button:
+            text: 'Initialize'
+#            on_release: app.open_settings()
+        Button:
+            text: 'Flow Rate'
+#            on_release: app.open_settings()
+        Button:
+            text: 'Direction'
+#            on_release: app.open_settings()
+        Button:
+            text: 'Run' 
+#            on_release: app.open_settings()
+        Button:
+            text: 'Stop' 
+#            on_release: app.open_settings()
+        Button:
+            text: 'Mode' 
+#            on_release: app.open_settings()
+        Button:
+            text: 'Sequence Editor' 
+#            on_release: app.open_settings()
+        Button:
+            text: 'Settings!'
+            on_release: app.open_settings()
+    Label:
+        text: 'Space Holder!!!!'
+    Label:
+        text: 'Space Holder1!!!!'
+    Label:
+        text: 'Space Holder2!!!!'
+    Label:
+        text: 'Space Holder3!!!!'
+    Label:
+        text: 'Space Holder4!!!!'
 ''')
 
 class Interface(BoxLayout):
     pass
 
-class sensorClass(object):
-	pass
-
-class pumpClass(object):
-	pass
-
 class Mainpanelapp(App):	
     def build(self):
         self.settings_cls = SettingsWithSidebar
         self.use_kivy_settings = False
-        setting = self.config.get('example', 'boolexample')
+        setting = self.config.get('operation', 'boolexample')
         return Interface()
 
     def build_config(self,config):
-	config.setdefaults('example', {
+	config.setdefaults('operation', {
 	    'boolexample': True,
 	    'numericexample' : 10,
 	    'optionsexample' : 'option2',
