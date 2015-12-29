cd829990d183c31374be075da9f1d4ab93bb82ee
Mike Parkin <mparkin@gmail.com>
2015-12-29 12:53:23 -0800

    Nelson house

--- senscollect.py
+++ senscollect.py
@@ -4,31 +4,36 @@ import datetime
 import serial
 
 class SensorRead():
-    data = " "
+    data = "none "
     filename = " "
     ser = 0
 
     def open(self,filename):
-        ser = serial.Serial('/dev/ttyACM0',38400)
-        filename = "{0}_{1}".format(filename,datetime.datetime.now().strftime('%d%m%y_%H%M%S'))
+        self.ser = serial.Serial('/dev/ttyACM0',38400)
+        self.filename = "{0}_{1}".format(filename,datetime.datetime.now().strftime('%d%m%y_%H%M%S'))
 
     def getData(self):
-	data = self.ser.readline()
+	self.data = self.ser.readline()
 
     def writeData(self):
-        f = open(self.filename, "w");
-        print data
+        f = open(self.filename, "a");
+        print self.data
 	print datetime.datetime.now().strftime('%d/%m/%Y %H:%M:%S')
-	f.write("{0},{1}\r\n".format(datetime.datetime.now().strftime('%d/%m/%Y %H:%M:%S'),data))
+	f.write("{0},{1}\r\n".format(datetime.datetime.now().strftime('%d/%m/%Y %H:%M:%S'),self.data))
         f.close()
 
-    def close():
-	self.file.close()
+    
+    def close(self):
+	self.f.close()
         self.ser.close()
 
 
 if __name__ == '__main__':
 	tst = SensorRead()
 	tst.open("test")
+        while(1):
+            tst.getData()
+            tst.writeData()
+
 	tst.close()
 	
